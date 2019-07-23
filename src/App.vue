3f79dd555680795b0f40f57d18979eeda0344bea
MiZone <mizonerom@hotmail.com>
2019-07-23 18:04:42 +0800

    add packet and tools

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,43 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <transition name="fade" mode="out-in">
+      <router-view />
+    </transition>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+<style lang="less">
+body {
+  margin: 0px;
+  padding: 0px;
+  font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB,
+    Microsoft YaHei, SimSun, sans-serif;
+  font-size: 14px;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+#app {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+}
+
+.reng-container {
+  margin-top: 20px;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.2s ease;
+}
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,40 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-Vue.config.productionTip = false
+// use element-ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// use vue-router
+import VueRouter from 'vue-router'
+
+// use moment
+import moment from 'moment'
+
+// use axios
+import {post,fetch,patch,put} from './service/requestApi.js'
+Vue.prototype.$post=post;
+Vue.prototype.$fetch=fetch;
+Vue.prototype.$patch=patch;
+Vue.prototype.$put=put;
+
+Vue.use(ElementUI)
+Vue.use(VueRouter)
+
+// handle admin, should release
+router.beforeEach((to,from,next) => {
+
+	if(to.path == '/login'){
+		sessionStorage.removeItem('Admin');
+  }
+  
+	let user = JSON.parse(sessionStorage.getItem('Admin'));
+	if(!user && to.path != '/Login'){
+		next({path: '/Login'});
+	} else{
+		next();
+	}
+})
 
 /* eslint-disable no-new */
 new Vue({
